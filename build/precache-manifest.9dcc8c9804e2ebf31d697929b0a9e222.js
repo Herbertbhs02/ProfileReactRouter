ae36837d11352f73b867f8edef2be7b46c09dcd9
Herbert Ssevume <you@example.com>
2020-04-09 19:52:39 +0100

    Connectbambi margin

---
+++ build/precache-manifest.9dcc8c9804e2ebf31d697929b0a9e222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e40ea34d7d2c5309b8d335260d278ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bde114f4b72d31187b4",
+    "url": "/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "7b24cf3e820f7efdb7d8",
+    "url": "/static/css/main.deaa93e7.chunk.css"
+  },
+  {
+    "revision": "6bde114f4b72d31187b4",
+    "url": "/static/js/2.7677c3db.chunk.js"
+  },
+  {
+    "revision": "7b24cf3e820f7efdb7d8",
+    "url": "/static/js/main.b72f0626.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
